c7b03d4c29dab6876a4c3e54eb29c4b482fcb154
Anusha Abhilash <anusha.vinaya97@gmail.com>
2023-07-13 17:33:20 +0200

    router

--- cookbook/src/App.jsx
+++ cookbook/src/App.jsx
@@ -1,18 +1,31 @@
 import Navigation from "./components/Navigation"
 // import Footer from "./components/Footer"
-// import RecipePage from "./components/RecipePage
+import RecipePage from "./components/RecipePage"
 import Hero from "./components/Hero"
 import Card from "./components/Card"
+
+import {Routes, Route} from 'react-router-dom';
 
 
 
 function App() {
 
 return (
+
     <div className="all">
-	<Navigation></Navigation>
+		<RecipePage/>
+	{/* <Navigation></Navigation>
 	<Hero></Hero>
-	<Card></Card>
+	<Card></Card>  */}
+
+		{/* <Routes> */}
+			{/* <Route path="/" element={}  />  */}
+{/* 	<Route path="/recipes/:recipesID" element={<RecipePage />}  />  */}
+		{/* <Route path="/recipes/" element={<RecipePage />}  /> */}
+			
+		{/* </Routes>
+ */}
+	
     </div>
 )
 }
--- cookbook/src/components/RecipePage.jsx
+++ cookbook/src/components/RecipePage.jsx
@@ -1,8 +1,37 @@
+import { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import RichText from './RichText';
+import {client} from '../contenful'
 
 const RecipePage = () => {
-  return (
-	<div>RecipePage</div>
-  )
-}
+  const [recipeItem, setRecipeItem] = useState(null);
+  const { recipeId } = useParams();
 
-export default RecipePage
+  useEffect(() => {
+    client
+      .getEntry(recipeId)
+      .then(response => setRecipeItem(response))
+      .catch(console.error);
+  }, []);
+
+  return (
+    <div>
+      {recipeItem && (
+        <>
+          <h2>{recipeItem.fields.title}</h2>
+          {recipeItem.fields.images.map(image => (
+            <img
+              className="img"
+              key={image.sys.id}
+              src={image.fields.file.url}
+              alt={recipeItem.fields.title}
+            />
+          ))}
+          <RichText document={recipeItem.fields.article} />
+        </>
+      )}
+    </div>
+  );
+};
+
+export default RecipePage;
