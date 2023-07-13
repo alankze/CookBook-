4564a5641ca41ed3f7d0bb97c6aff956ce242cdc
WENJUN <vesper0@163.com>
2023-07-13 14:51:57 +0200

    add footer structure

--- cookbook/src/App.jsx
+++ cookbook/src/App.jsx
@@ -1,20 +1,19 @@
-import Navigation from "./components/Navigation"
+import Navigation from "./components/Navigation";
 // import Footer from "./components/Footer"
 // import RecipePage from "./components/RecipePage
-import Hero from "./components/Hero"
-import Card from "./components/Card"
-
-
+import Hero from "./components/Hero";
+import Card from "./components/Card";
+import Footer from "./components/Footer";
 
 function App() {
-
-return (
+  return (
     <div className="all">
-	<Navigation></Navigation>
-	<Hero></Hero>
-	<Card></Card>
+      <Navigation></Navigation>
+      <Hero></Hero>
+      <Card></Card>
+      <Footer />
     </div>
-)
+  );
 }
 
-export default App
+export default App;
--- cookbook/src/components/Footer.jsx
+++ cookbook/src/components/Footer.jsx
@@ -1,9 +1,31 @@
-
+import Logo from "../img/Logo.png";
+import dribbble from "../img/dribbble.png";
+import facebook from "../img/facebook.png";
+import twitter from "../img/twitter.png";
+import youtube from "../img/youtube.png";
+import instagram from "../img/instagram.png";
 
 const Footer = () => {
   return (
-	<div>Footer</div>
-  )
-}
+    <footer>
+      <div>
+        <img src={Logo} />
+      </div>
+      <div>
+        <h3>Stay in touch</h3>
+        <div>
+          <img src={dribbble} />
+          <img src={facebook} />
+          <img src={twitter} />
+          <img src={youtube} />
+          <img src={instagram} />
+        </div>
+      </div>
+      <div>
+        <p> All Rights Reserved.</p>
+      </div>
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
